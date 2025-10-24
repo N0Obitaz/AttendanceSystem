18387045a97d6b50bf60d745bc1f86bd03f49f02
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-10-24 13:06:20 +0800

    Global Shared Layout(Will be removed)

---
+++ Pages/student_view/home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AttendanceSystem.Pages.student_view
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
