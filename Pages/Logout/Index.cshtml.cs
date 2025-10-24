f19793b5c6969db6d0c49be4b29da040ed953089
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-10-24 15:21:42 +0800

    Changed to Onget

--- Pages/Logout/Index.cshtml.cs
+++ Pages/Logout/Index.cshtml.cs
@@ -6,7 +6,19 @@ namespace AttendanceSystem.Pages
 {
     public class LogoutModel : PageModel
     {
-        public async Task<IActionResult> OnPostAsync()
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    // Sign out the user from the "CustomSession" cookie
+        //    await HttpContext.SignOutAsync("CustomSession");
+
+        //    // Optional: clear any additional cookies if needed
+        //    // Response.Cookies.Delete(".AspNetCore.CustomSession");
+
+        //    // Redirect to login page after logout
+        //    return RedirectToPage("/Index");
+        //}
+
+        public async Task<IActionResult> OnGetAsync()
         {
             // Sign out the user from the "CustomSession" cookie
             await HttpContext.SignOutAsync("CustomSession");
