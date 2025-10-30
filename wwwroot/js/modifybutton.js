85f67763ec22167374220c2119e1b37ab47036b6
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-10-30 23:45:22 +0800

    Added Function For Cancel Button

---
+++ wwwroot/js/modifybutton.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("btnLocation").addEventListener("click", async () => {
+    var locationDisplay = document.getElementById("locationDiv");
+    locationDisplay.classList.remove("d-none");
+
+    var cancelBtn = document.getElementById("cancelBtn");
+    cancelBtn.classList.remove("d-none")
+});
