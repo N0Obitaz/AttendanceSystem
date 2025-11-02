9188b456825b185e17dc260144fd64cab89dd6f4
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-11-02 23:16:56 +0800

    Pagination Function

---
+++ wwwroot/js/Pagination.js
@@ -0,0 +1,42 @@
+﻿let currentPage = 1;
+const rowsPerPage = 10;
+const allRows = document.querySelectorAll('.row');
+let filteredRows = Array.From(allRows);
+let totalPages = Math.ceil(filteredRows.length / rowsPerPage);
+
+function displayPage(page) {
+
+    allRows.forEach(page => page.style.display = 'none');
+
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    for (let i = start; i < end && i < filteredRows.length; i++)
+    {
+        filteredRows[i].style.display = 'block';
+    }
+    document.getElementById('page-info').textContent =
+        `Page ${page} of ${totalPages || 1}`;
+
+
+}
+
+function nextPage() {
+    if (currentPage < totalPages) {
+        currentPages++;
+        showPage(currentPage);
+
+    }
+
+
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        showPage(currentPage);
+    }
+
+}
+
+
