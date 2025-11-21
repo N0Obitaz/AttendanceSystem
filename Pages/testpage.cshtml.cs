62ee553214c14fc198bf0f66013c832e23a63860
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-11-21 21:42:57 +0800

    installed tailwindcss 3

---
+++ Pages/testpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AttendanceSystem.Pages
+{
+    public class testpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml', // For Razor Pages
+        './Views/**/*.cshtml', // For MVC    
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
