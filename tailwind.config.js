78421a2fb09a1a6ec8d4e5113c992ee69d95929a
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-11-22 08:33:45 +0800

    fixed using colors from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@
 module.exports = {
     content: [
         './Pages/**/*.cshtml',
+        "./Pages/Shared/*.cshtml",
+        "./*cshtml"
         
     ],
   theme: {
       extend: {
           backgroundImage: {
               'custom-teal-gradient': 'linear-gradient(to right, #85ad74, #005540)',
+          },
+          colors: {
+              'greens': "#000000",
+              'brand-teal': '#85ad74',
+              'brand-dark-teal': '#005540',
           }
+
       },
   },
   plugins: [],
