1430df47b2e4eeb3ba0942ed68c41e51b11024bb
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-12-04 00:30:06 +0800

    Updates

--- Pages/Admin_view/Dashboard/Index.cshtml.cs
+++ Pages/Admin_view/Dashboard/Index.cshtml.cs
@@ -20,9 +20,80 @@ namespace AttendanceSystem.Pages.Admin_view.Dashboard
         {
             _context = context;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; }
+
+        [BindProperty]
+        public int? Present { get; set; }
+
+        [BindProperty]
+        public int? Absent{ get; set; }
+        [BindProperty]
+        public int? Late { get; set; }
+
+        [BindProperty]
+        public int? FilteredPresent { get; set; }
+        [BindProperty]
+        public int? FilteredAbsent{ get; set; }
+        [BindProperty]
+        public int? FilteredLate{ get; set; }
         public void OnGet()
         {
 
+            var today = DateTime.UtcNow.Day;
+
+            Present = _context.Attendances
+                .Count(a => a.Status == "Present");
+
+            Absent = _context.Attendances
+                .Count(a => a.Status == "Absent");
+
+            Late = _context.Attendances
+                .Count(a => a.Status == "Late");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostFilterAsync(DateTime startDate, DateTime endDate)
+        {
+           
+            startDate = DateTime.SpecifyKind(startDate, DateTimeKind.Utc);
+            endDate = DateTime.SpecifyKind(endDate, DateTimeKind.Utc);
+
+          
+            StartDate = startDate;
+            EndDate = endDate;
+
+            if (startDate > endDate)
+            {
+                ModelState.AddModelError(string.Empty, "Start date must be earlier than end date.");
+                return Page();
+            }
+
+            var today = DateTime.SpecifyKind(DateTime.Today, DateTimeKind.Utc);
+
+            Present = _context.Attendances
+                .Count(a => a.Status == "Present");
+
+            Absent = _context.Attendances
+                .Count(a => a.Status == "Absent");
+
+            Late = _context.Attendances
+                .Count(a => a.Status == "Late");
+
+            FilteredPresent = _context.Attendances
+                .Count(a => a.Status == "Present" && a.Date.Date >= startDate.Date && a.Date.Date <= endDate.Date);
+
+            FilteredAbsent = _context.Attendances
+                .Count(a => a.Status == "Absent" && a.Date.Date >= startDate.Date && a.Date.Date <= endDate.Date);
+
+            FilteredLate = _context.Attendances
+                .Count(a => a.Status == "Late" && a.Date.Date >= startDate.Date && a.Date.Date <= endDate.Date);
+
+            return Page();
         }
     }
 }
