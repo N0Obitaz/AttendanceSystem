66b025860b6b8ff966e206f374694393b1b344a8
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-03 20:54:15 +0800

    feat: added extention to handle byte size serialized cached data

---
+++ Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace AttendanceSystem.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, 
+            string recordId, 
+            T data, 
+            TimeSpan? absoluteExpiretime = null, 
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpiretime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+
+
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if(jsonData is null)
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
